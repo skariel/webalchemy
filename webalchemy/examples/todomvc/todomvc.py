2df255df99e63ec3db32afa9466911f0b40fb66f
skariel <skariel@gmail.com>
2013-12-29 16:15:39 +0200

    mvc now working!

--- webalchemy/examples/todomvc/todomvc.py
+++ webalchemy/examples/todomvc/todomvc.py
@@ -6,140 +6,92 @@ class AppTodoMvc:
         self.rdoc = kwargs['remote_document']
         self.controller = controller(self.rdoc)
         self.controller.bind_html(kwargs['main_html'])
-        self.js_renderpage = self.rdoc.jsfunction('''
-            var list = #{self.controller.e.todo_list};
+        self.rdoc.JS('''
 
-            #{self.controller.model}.total = list.children.length;
-            #{self.controller.model}.completed = 0;
-            for (var i=0; i<#{self.controller.model}.total; i++)
-                if (list.children[i].classList.contains('completed')) #{self.controller.model}.completed++;
+            // the data model
 
-            #{self.controller.model}.remaining = #{self.controller.model}.total - #{self.controller.model}.completed;
+            #{self.controller.model} = {
 
-            //////////////////////////////////////////////// STORAGE STUFF /////////////////////////////////////////////
-            //
-            // drop new data to disk...
-//            if (#{self.controller.model}.total == 0) localStorage['ult'] = 0;
-//            if (!('ult' in localStorage))
-//                localStorage['ult'] = 0;
-//
-//            ults_in_list = []
-//            for (var i=0; i<list.children.length; i++) {
-//                var it = list.children[i];
-//                if (!('ult' in it)) {
-//                    it.ult = localStorage['ult'];
-//                    localStorage['ult'] = 1+parseInt(localStorage['ult']);
-//                }
-//                localStorage[it.ult] = it.app.tasktextlabel.textContent;
-//                localStorage[it.ult+'_completed'] = it.classList.contains('completed');
-//                ults_in_list.push(it.ult);
-//                ults_in_list.push(it.ult+'_completed');
-//            }
-//            // delete old data from disk...
-//            for (var key in localStorage) {
-//                if (key == 'ult') continue;
-//                if (ults_in_list.indexOf(key)<0)
-//                    localStorage.removeItem(key);
-//            }
-        ''')
-        self.js_createitem = self.rdoc.jsfunction('text', self.createitem)
+                itemlist : JSON.parse(localStorage.getItem('webatodomvcdata')) || [],
 
-        # binding elements
-        self.controller.e.new_todo.events.add(keyup=self.rdoc.jsfunction('event', '''
-            if (event.keyCode == #{self.rdoc.KeyCode.ESC}) #{self.controller.e.new_todo}.blur();
-            if (event.keyCode == #{self.rdoc.KeyCode.ENTER}) {
-                if (#{self.controller.e.new_todo}.value.trim()!='')
-                    #{self.js_createitem}(#{self.controller.e.new_todo}.value);
-                #{self.controller.e.new_todo}.blur();
-            }
-        '''))
-        self.controller.e.toggle_all.events.add(click=self.rdoc.jsfunction('''
-            var list = #{self.controller.e.todo_list};
-            for (var i=0; i<list.children.length; i++) {
-                it = list.children[i];
-                if (#{self.controller.e.toggle_all}.checked) {
-                    it.classList.add('completed');
-                } else {
-                    it.classList.remove('completed');
+                set_all_completed : function(comp_or_not) {
+                    for (var i=0, ill=this.itemlist.length; i<ill; i++)
+                        this.itemlist[i].completed = comp_or_not;
+                    this.persist();
+                },
+
+                remove_completed : function() {
+                    var new_list = [];
+                    for (var i=0, ill=this.itemlist.length; i<ill; i++)
+                        if (!this.itemlist[i].completed)
+                            new_list.push(this.itemlist[i]);
+                    this.itemlist = new_list;
+                    this.persist();
+                },
+
+                remove_item : function(i) {
+                    this.itemlist.splice(i,1);
+                    this.persist();
+                },
+
+                add_item : function(txt) {
+                    this.itemlist.push({
+                        text:txt,
+                        completed:false
+                    });
+                    this.persist();
+                },
+
+                toggle_item_completed : function(i) {
+                    this.itemlist[i].completed = !this.itemlist[i].completed;
+                    this.persist();
+                },
+
+                persist : function() {
+                    localStorage.setItem('webatodomvcdata', JSON.stringify(this.itemlist));
                 }
             }
-        '''))
-        self.controller.e.clear_completed.events.add(click=self.rdoc.jsfunction('''
-            var items = [];
-            var list = #{self.controller.e.todo_list};
-            for (var i=0; i<list.children.length; i++)
-                items.push(list.children[i]);
-            for (var i=0; i<items.length; i++) {
-                var it = items[i];
-                if (it.classList.contains('completed'))
-                    list.removeChild(it);
+
+            // viemodel
+
+            #{self.controller.viewmodel} = {
+
+                itembeingedited : null,
+
+                new_item_keyup : function(e) {
+                    if (e.keyCode == #{self.rdoc.KeyCode.ESC}) e.target.blur();
+                    if (e.keyCode == #{self.rdoc.KeyCode.ENTER}) {
+                        if (e.target.value.trim()!='')
+                            e.target.app.model.add_item(e.target.value);
+                        e.target.blur();
+                    }
+                },
+
+                edit_keyup : function(e, model, i) {
+                    if (e.keyCode == #{self.rdoc.KeyCode.ESC}) this.itembeingedited = undefined;
+                    if (e.keyCode != #{self.rdoc.KeyCode.ENTER}) return;
+                    this.itembeingedited = undefined;
+                    model.itemlist[i].text = e.target.value;
+                    if (e.target.value.trim()=='') model.remove_item(i);
+                },
+
+                editing_item_changed : function(e, model, i, tothisitem) {
+                    if (tothisitem) {
+                        e.focus();
+                        e.value=model.itemlist[i].text;
+                        this.persist();
+                    }
+                    else
+                        e.blur();
+                }
             }
-        '''))
-
-        # loading from disk...
-        # self.rdoc.JS('''
-        #     var list = #{self.controller.e.todo_list}.children;
-        #     for (var key in localStorage) {
-        #         if (key == 'ult') continue;
-        #         if (key.indexOf('_')>=0) continue;
-        #         if (['watch', 'unwatch'].indexOf(key)>=0) continue;
-        #         // we have a real item!
-        #         #{self.js_createitem}(localStorage[key]);
-        #         item = list[list.length-1];
-        #         item.app.ult = key;
-        #         if (localStorage[key+'_completed'] == 'true')
-        #             item.classList.add('completed');
-        #     }
-        # ''')
-
-        self.rdoc.JS('''
-            // some dmmy model
-            #{self.controller.model}.itemlist=[
-                {
-                    text:'hi1',
-                    completed:true
-                },
-                {
-                    text:'hi2',
-                    completed:false
-                },
-                {
-                    text:'hi3',
-                    completed:true
-                }]
         ''')
-        self.controller.prerender = self.js_renderpage
+
+        self.controller.prerender = self.rdoc.jsfunction('''
+            #{self.controller.model}.completed = 0;
+            for (var i=0, il=#{self.controller.model}.itemlist, ill=il.length; i<ill; i++)
+                if (il[i].completed) #{self.controller.model}.completed++;
+            #{self.controller.model}.remaining = #{self.controller.model}.itemlist.length - #{self.controller.model}.completed;
+        ''')
+
         self.controller.call_on_request_frame()
-
-    def createitem(self, text):
-        li = self.controller.e.todo_list.element('li', app=True)
-        div = li.element('div')
-        div.cls.append('view')
-        completedcheckbox = div.element('input')
-        completedcheckbox.att.type = 'checkBox'
-        completedcheckbox.cls.append('toggle')
-        tasktextlabel = div.element('label', text)
-        li.app.tasktextlabel = tasktextlabel
-        destroybutton = div.element('button')
-        destroybutton.cls.append('destroy')
-        edittaskinput = li.element('input')
-        edittaskinput.cls.append('edit')
-        edittaskinput.prop.value = text
-
-        # binding events...
-        completedcheckbox.events.add(click=lambda: li.cls.toggle('completed'))
-        tasktextlabel.events.add(dblclick=lambda: li.cls.append('editing'))
-        tasktextlabel.events.add(dblclick=lambda: edittaskinput.cal.focus())
-        edittaskinput.events.add(blur=lambda: li.cls.remove('editing'))
-        destroybutton.events.add(click=lambda: li.remove())
-        edittaskinput.events.add(keyup=self.rdoc.jsfunction('event', '''
-            if (event.keyCode == #{self.rdoc.KeyCode.ESC}) #{edittaskinput}.blur();
-            if (event.keyCode != #{self.rdoc.KeyCode.ENTER}) return;
-            #{edittaskinput}.blur();
-            #{tasktextlabel}.textContent = #{edittaskinput}.value
-            if (#{edittaskinput}.value.trim()!='') return;
-            #{li}.parentNode.removeChild(#{li});
-        '''))
-        self.controller.bind('toggled_style', li, '''((element.classList.contains("completed"))&&(location.hash=="#/active"))||
-                ((!(element.classList.contains("completed")))&&(location.hash=="#/completed"))''', '"display"', '"none"')
-        self.controller.bind('property', completedcheckbox, li.varname+'.classList.contains("completed")', '"checked"');
--- webalchemy/mvc.py
+++ webalchemy/mvc.py
@@ -8,22 +8,23 @@ class controller:
             pass
         self.e = c()
         self.model = self.rdoc.dict()
+        self.viewmodel = self.rdoc.dict()
 
-        self.toggled_class = self.rdoc.jsfunction('model', 'element', 'fnc', 'cls', body='''
-            if (fnc(model, element))
+        self.toggled_class = self.rdoc.jsfunction('viewmodel', 'model', 'element', 'newval', 'i', 'cls', body='''
+            if (newval)
                 element.classList.add(cls);
             else
                 element.classList.remove(cls);
             ''')
 
-        self.toggled_style = self.rdoc.jsfunction('model', 'element', 'fnc', 'style_att', 'style_opt', body='''
-            if (fnc(model, element))
+        self.toggled_style = self.rdoc.jsfunction('viewmodel', 'model', 'element', 'newval', 'i', 'style_att', 'style_opt', body='''
+            if (newval)
                 element.style[style_att]=style_opt;
             else
                 element.style[style_att]='';
         ''')
 
-        self.repeat_i_in = self.rdoc.jsfunction('model', 'element', 'fnc', body='''
+        self.repeat_i_in = self.rdoc.jsfunction('viewmodel', 'model', 'element', 'newval', 'i', body='''
             if (typeof element.app.template=='undefined') {
                 element.app.template = element.children[0];
                 element.removeChild(element.children[0]);
@@ -31,7 +32,7 @@ class controller:
 
             var ec = element.children;
             var ecl = ec.length;
-            var arr = fnc(model, element);
+            var arr = newval;
             var arrl = arr.length;
             var c = arrl-ecl;
             var te = element.app.template;
@@ -41,14 +42,18 @@ class controller:
             var eid = String(element.getAttribute('id'));
             var tid = String(te.getAttribute('id'));
             var id = eid+'_'+tid+'_';
+
             function copy_app_execute_controller(to, from, i) {
                 if (typeof to.app=='undefined')
                     to.app = {};
                 if ((typeof from.app!='undefined')&&
-                    (typeof from.app.execute_controller!='undefined'))
+                    (typeof from.app.execute_controller!='undefined')) {
                     to.app.execute_controller = from.app.execute_controller;
-                to.app.model = model;
+                    to.app.oldval = new Array(from.app.oldval.length);
+                }
                 to.app.i = i;
+                to.app.model = model;
+                to.app.viewmodel = viewmodel;
 
                 var fc = from.children;
                 if (typeof fc=='undefined')
@@ -58,11 +63,12 @@ class controller:
                 for (var ci=0; ci<l; ci++)
                     copy_app_execute_controller(tc[ci], fc[ci], i)
             }
+
             if (c>0)
                 // add new elements
                 for (var i=0; i<c; i++) {
                     var e = te.cloneNode(true);
-                    var eid = id+i;
+                    var eid = id+(i+ecl);
                     e.setAttribute('id', eid);
                     e.innerHTML = ih;
                     e.app = {};
@@ -71,30 +77,39 @@ class controller:
                 }
             else
                 // remove un-needed elements
-                for (var i=c; i<0; i++) {
-                    console.log('removing...')
-                    element.removeChild(element.children[element.children.length-1]);
-                    console.log('removed!')
-                }
-
+                for (var i=c; i<0; i++)
+                    element.removeChild(ec[ec.length-1]);
         ''')
 
-        self.property = self.rdoc.jsfunction('model', 'element', 'fnc', 'prop', body='''
-            element[prop]=fnc(model, element);
+        self.property = self.rdoc.jsfunction('viewmodel', 'model', 'element', 'newval', 'i', 'prop', body='''
+            element[prop] = newval;
         ''')
+
+        self.code = self.rdoc.jsfunction('viewmodel', 'model', 'element', 'newval', 'i', 'code', body='''
+            eval(code);
+        ''')
+
+        self.ismuttable = self.rdoc.jsfunction('test', body='''return (test && (typeof test == 'object' || typeof test == 'function'))''')
 
     def execute(self):
         self.rdoc.JS('''
-            for (var i=0, a=document.all, c=a.length; i<c; i++)
-                if ((typeof a[i]!='undefined')&&(typeof a[i].app!='undefined')&&
-                (typeof a[i].app.execute_controller!='undefined'))
-                    for (var j=0; k=a[i].app.execute_controller.length, j<k; j++)
-                        a[i].app.execute_controller[j](#{self.model}, a[i]);
+            function crawl_element(e) {
+                if ((typeof e!='undefined')&&(typeof e.app!='undefined')) {
+                    e.app.model = #{self.model};
+                    e.app.viewmodel = #{self.viewmodel};
+                    if (typeof e.app.execute_controller!='undefined')
+                        for (var j=0, k=e.app.execute_controller, kl=k.length; j<kl; j++)
+                            k[j](#{self.viewmodel}, #{self.model}, e, j, e.app.i);
+                }
+                for (var i=0, ec=e.children, ecl=ec.length; i<ecl; i++)
+                    crawl_element(ec[i]);
+            }
+            crawl_element(document.body);
         ''')
 
     def bind(self, at, element, code, *varargs):
         if isinstance(code, str):
-            fnc = self.rdoc.jsfunction('model', 'element', body='return '+code)
+            fnc = self.rdoc.jsfunction('viewmodel', 'model', 'element', 'i', body='return '+code)
         else:
             fnc = code
         whattodo = getattr(self, at)
@@ -107,13 +122,21 @@ class controller:
                 #{element}.app = {};
             if (typeof #{element}.app.execute_controller == 'undefined')
                 #{element}.app.execute_controller = [];
-            if (typeof #{element}.app.model == 'undefined')
-                #{element}.app.model = #{self.model};
+            if (typeof #{element}.app.oldval == 'undefined')
+                #{element}.app.oldval = [];
+            #{element}.app.oldval.push(undefined);
+            #{element}.app.model = #{self.model};
+            #{element}.app.viewmodel = #{self.viewmodel};
             #{element}.app.execute_controller.push(
-                function(model, element) {
-                    #{whattodo}(model, element, #{fnc}'''+s+''');
+                function(viewmodel, model, element, j, i) {
+                    var newval = #{fnc}(viewmodel, model, element, i);
+                    if ((#{self.ismuttable}(newval)) || (newval != element.app.oldval[j])) {
+                        #{whattodo}(viewmodel, model, element, newval, i'''+s+''');
+                        element.app.oldval[j] = newval;
+                    }
                 });
         ''', encapsulate_strings=False)
+        # TODO: remove above closure, for performance... maybe just turn the jsfunctions into strings to be embedded?
 
     def bind_html(self, html):
         class MyHTMLParser(HTMLParser):
@@ -129,8 +152,8 @@ class controller:
                         self.ctrl.rdoc.JS('''
                             if (typeof #{e}.app=='undefined')
                                 #{e}.app = {};
-                            if (typeof #{e}.app.model=='undefined')
-                                #{e}.app.model = #{self.ctrl.model};
+                            #{e}.app.model = #{self.ctrl.model};
+                            #{e}.app.viewmodel = #{self.ctrl.viewmodel};
                         ''')
                         setattr(self.ctrl.e, eid.replace('-', '_').strip().replace(' ', '_'), e)
                     elif attr[0].startswith('weba-'):
