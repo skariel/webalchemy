d42bf84bf27a72d686141e73a60d90fa92eaec0e
Andreas Schlapsi <andreas.schlapsi@gmail.com>
2014-01-25 13:19:28 +0100

    Simplified unit tests

--- webalchemy/test/configtests.py
+++ webalchemy/test/configtests.py
@@ -34,27 +34,18 @@ class TestConfig(unittest.TestCase):
 
     def test_default_settings(self):
         cfg = config.read_config_from_app(AppDummyWithoutConfig)
-        self.assertEqual('static', cfg['SERVER_STATIC_PATH'])
-        self.assertEqual('websocket', cfg['SERVER_WS_ROUTE'])
-        self.assertEqual('127.0.0.1', cfg['SERVER_HOST'])
-        self.assertEqual(8080, cfg['SERVER_PORT'])
-        self.assertIsNone(cfg['SERVER_MONITORED_FILES'])
-        self.assertIsNone(cfg['SERVER_SSL_CERT'])
-        self.assertIsNone(cfg['SERVER_SSL_KEY'])
-        self.assertIsNone(cfg['SERVER_MAIN_ROUTE'])
-        self.assertIsNone(cfg['FREEZE_OUTPUT'])
+        self.verify_config(cfg, AppDummyWithoutConfig)
 
     def test_overridden_settings(self):
         cfg = config.read_config_from_app(AppDummyWithConfig)
-        self.assertEqual('public', cfg['SERVER_STATIC_PATH'])
-        self.assertEqual('websocket', cfg['SERVER_WS_ROUTE'])
-        self.assertEqual('127.0.0.1', cfg['SERVER_HOST'])
-        self.assertEqual(3000, cfg['SERVER_PORT'])
-        self.assertIsNone(cfg['SERVER_MONITORED_FILES'])
-        self.assertEqual('mydomain.crt', cfg['SERVER_SSL_CERT'])
-        self.assertEqual('mydomain.key', cfg['SERVER_SSL_KEY'])
-        self.assertIsNone(cfg['SERVER_MAIN_ROUTE'])
-        self.assertEqual('output.html', cfg['FREEZE_OUTPUT'])
+        self.verify_config(cfg, AppDummyWithConfig)
+
+    def verify_config(self, cfg, app):
+        for key, value in config.DEFAULT_SETTINGS.items():
+            if hasattr(app, 'config') and key in app.config:
+                self.assertEqual(cfg[key], app.config[key])
+            else:
+                self.assertEqual(cfg[key], config.DEFAULT_SETTINGS[key])
 
 
 if __name__ == '__main__':
